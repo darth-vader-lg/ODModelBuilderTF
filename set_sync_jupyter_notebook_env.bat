800e44146057566274a78230300f50b3ffc797b0
darth-vader-lg <luigi.generale@gmail.com>
2021-04-13 17:57:22 +0200

    Modificato il batch di impostazione variabile d'ambiente del sincronizzatore py / notebook: falliva se era compilata solo la versione debug.

--- set_sync_jupyter_notebook_env.bat
+++ set_sync_jupyter_notebook_env.bat
@@ -2,11 +2,13 @@ if not exist "..\SyncJupyterNotebook\" (
   echo The submodule SyncJupyterNotebook doesn't exist. Please add it to the solution
   exit /B 1
 )
+setlocal enabledelayedexpansion
 set SyncJupyterNotebook="..\SyncJupyterNotebook\bin\Release\net5.0\SyncJupyterNotebook.exe"
-if not exist %SyncJupyterNotebook% (
+if not exist !SyncJupyterNotebook! (
   set SyncJupyterNotebook="..\SyncJupyterNotebook\bin\Debug\net5.0\SyncJupyterNotebook.exe"
-  if not exist %SyncJupyterNotebook% (
+  if not exist !SyncJupyterNotebook! (
     echo the SyncJupyterNotebook executable doesn't exist. Please build the solution.
     exit /B 1
   )
 )
+endlocal & set SyncJupyterNotebook=%SyncJupyterNotebook%
