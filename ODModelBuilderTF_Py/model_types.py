495e7b9c82a5d21e0503089d7256c6076f070549
darth-vader-lg <luigi.generale@gmail.com>
2021-10-01 17:11:36 +0200

    Can download pre-trained models as gz archives or single files.

--- ODModelBuilderTF_Py/model_types.py
+++ ODModelBuilderTF_Py/model_types.py
@@ -1,147 +1,186 @@
+import  os
+
 """ List of the available models and their definitions """
 models = {
+    # TensorFlow2 ModelZoo
     'CenterNet HourGlass104 512x512': {
         'dir_name': 'centernet_hg104_512x512_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200713/centernet_hg104_512x512_coco17_tpu-8.tar.gz'
     },
     'CenterNet HourGlass104 1024x1024': {
         'dir_name': 'centernet_hg104_1024x1024_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200713/centernet_hg104_1024x1024_coco17_tpu-32.tar.gz'
     },
     'CenterNet Resnet50 V1 FPN 512x512': {
         'dir_name': 'centernet_resnet50_v1_fpn_512x512_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/centernet_resnet50_v1_fpn_512x512_coco17_tpu-8.tar.gz'
     },
     'CenterNet Resnet101 V1 FPN 512x512': {
         'dir_name': 'centernet_resnet101_v1_fpn_512x512_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/centernet_resnet101_v1_fpn_512x512_coco17_tpu-8.tar.gz'
     },
     'CenterNet Resnet50 V2 512x512': {
         'dir_name': 'centernet_resnet50_v2_512x512_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/centernet_resnet50_v2_512x512_coco17_tpu-8.tar.gz'
     },
     'CenterNet MobileNetV2 FPN 512x512': {
         'dir_name': 'centernet_mobilenetv2_fpn_od',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20210210/centernet_mobilenetv2fpn_512x512_coco17_od.tar.gz'
     },
     'EfficientDet D0 512x512': {
         'dir_name': 'efficientdet_d0_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz'
     },
     'EfficientDet D1 640x640': {
         'dir_name': 'efficientdet_d1_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d1_coco17_tpu-32.tar.gz'
     },
     'EfficientDet D2 768x768': {
         'dir_name': 'efficientdet_d2_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d2_coco17_tpu-32.tar.gz'
     },
     'EfficientDet D3 896x896': {
         'dir_name': 'efficientdet_d3_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d3_coco17_tpu-32.tar.gz'
     },
     'EfficientDet D4 1024x1024': {
         'dir_name': 'efficientdet_d4_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d4_coco17_tpu-32.tar.gz'
     },
     'EfficientDet D5 1280x1280': {
         'dir_name': 'efficientdet_d5_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d5_coco17_tpu-32.tar.gz'
     },
     'EfficientDet D6 1280x1280': { # Really speaking it's 1408
         'dir_name': 'efficientdet_d6_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d6_coco17_tpu-32.tar.gz'
     },
     'EfficientDet D7 1536x1536': {
         'dir_name': 'efficientdet_d7_coco17_tpu-32',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d7_coco17_tpu-32.tar.gz'
     },
     'SSD MobileNet v2 320x320': { # Really speaking it's 300
         'dir_name': 'ssd_mobilenet_v2_320x320_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_320x320_coco17_tpu-8.tar.gz'
     },
     'SSD MobileNet V1 FPN 640x640': {
         'dir_name': 'ssd_mobilenet_v1_fpn_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v1_fpn_640x640_coco17_tpu-8.tar.gz'
     },
     'SSD MobileNet V2 FPNLite 320x320': {
         'dir_name': 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
     },
     'SSD MobileNet V2 FPNLite 640x640': {
         'dir_name': 'ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8.tar.gz'
     },
     'SSD ResNet50 V1 FPN 640x640 (RetinaNet50)': {
         'dir_name': 'ssd_resnet50_v1_fpn_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.tar.gz'
     },
     'SSD ResNet50 V1 FPN 1024x1024 (RetinaNet50)': {
         'dir_name': 'ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8.tar.gz'
     },
     'SSD ResNet101 V1 FPN 640x640 (RetinaNet101)': {
         'dir_name': 'ssd_resnet101_v1_fpn_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet101_v1_fpn_640x640_coco17_tpu-8.tar.gz'
     },
     'SSD ResNet101 V1 FPN 1024x1024 (RetinaNet101)': {
         'dir_name': 'ssd_resnet101_v1_fpn_1024x1024_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet101_v1_fpn_1024x1024_coco17_tpu-8.tar.gz'
     },
     'SSD ResNet152 V1 FPN 640x640 (RetinaNet152)': {
         'dir_name': 'ssd_resnet152_v1_fpn_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet152_v1_fpn_640x640_coco17_tpu-8.tar.gz'
     },
     'SSD ResNet152 V1 FPN 1024x1024 (RetinaNet152)': {
         'dir_name': 'ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN ResNet50 V1 640x640': {
         'dir_name': 'faster_rcnn_resnet50_v1_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_640x640_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN ResNet50 V1 1024x1024': {
         'dir_name': 'faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN ResNet50 V1 800x1333': {
         'dir_name': 'faster_rcnn_resnet50_v1_800x1333_coco17_gpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet50_v1_800x1333_coco17_gpu-8.tar.gz'
     },
     'Faster R-CNN ResNet101 V1 640x640': {
         'dir_name': 'faster_rcnn_resnet101_v1_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet101_v1_640x640_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN ResNet101 V1 1024x1024': {
         'dir_name': 'faster_rcnn_resnet101_v1_1024x1024_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet101_v1_1024x1024_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN ResNet101 V1 800x1333': {
         'dir_name': 'faster_rcnn_resnet101_v1_800x1333_coco17_gpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet101_v1_800x1333_coco17_gpu-8.tar.gz'
     },
     'Faster R-CNN ResNet152 V1 640x640': {
         'dir_name': 'faster_rcnn_resnet101_v1_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet152_v1_640x640_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN ResNet152 V1 1024x1024': {
         'dir_name': 'faster_rcnn_resnet152_v1_1024x1024_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet152_v1_1024x1024_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN ResNet152 V1 800x1333': {
         'dir_name': 'faster_rcnn_resnet152_v1_800x1333_coco17_gpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_resnet152_v1_800x1333_coco17_gpu-8.tar.gz'
     },
     'Faster R-CNN Inception ResNet V2 640x640': {
         'dir_name': 'faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8.tar.gz'
     },
     'Faster R-CNN Inception ResNet V2 1024x1024': { # Really speaking it's 800x1333
         'dir_name': 'faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8.tar.gz'
     },
     'Mask R-CNN Inception ResNet V2 1024x1024': {
         'dir_name': 'mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8',
+        'file_name': os.path.join('saved_model', 'saved_model.pb'),
         'download_path': 'http://download.tensorflow.org/models/object_detection/tf2/20200711/mask_rcnn_inception_resnet_v2_1024x1024_coco17_gpu-8.tar.gz'
     },
 }
--- ODModelBuilderTF_Py/pretrained_model.py
+++ ODModelBuilderTF_Py/pretrained_model.py
@@ -13,19 +13,25 @@ def download_pretrained_model(prm: BaseParameters):
     prm     -- the base parameters
     """
     pre_trained_model_dir = os.path.join(prm.pre_trained_model_base_dir, prm.model['dir_name'])
-    if (not os.path.exists(pre_trained_model_dir) or not os.path.exists(os.path.join(pre_trained_model_dir, 'checkpoint', 'checkpoint'))):
+    if (not os.path.exists(pre_trained_model_dir) or not os.path.exists(os.path.join(pre_trained_model_dir, prm.model['file_name']))):
         if (not os.path.exists(prm.pre_trained_model_base_dir)):
             os.mkdir(prm.pre_trained_model_base_dir)
-        pre_trained_model_file = pre_trained_model_dir + '.tar.gz'
-        print(f'Downloading the pre-trained model {str(Path(pre_trained_model_file).name)}...')
-        import tarfile
-        request.urlretrieve(prm.model['download_path'], pre_trained_model_file) # TODO: show progress
-        print('Done.')
-        print(f'Extracting the pre-trained model {str(Path(pre_trained_model_file).name)}...')
-        tar = tarfile.open(pre_trained_model_file)
-        tar.extractall(prm.pre_trained_model_base_dir)
-        tar.close()
-        os.remove(pre_trained_model_file)
+        if (Path(prm.model['download_path']).suffix.casefold() == ".gz".casefold()):
+            pre_trained_model_file = pre_trained_model_dir + '.tar.gz'
+            print(f'Downloading the pre-trained model {str(Path(pre_trained_model_file).name)}...')
+            import tarfile
+            request.urlretrieve(prm.model['download_path'], pre_trained_model_file) # TODO: show progress
+            print('Done.')
+            print(f'Extracting the pre-trained model {str(Path(pre_trained_model_file).name)}...')
+            tar = tarfile.open(pre_trained_model_file)
+            tar.extractall(prm.pre_trained_model_base_dir)
+            tar.close()
+            os.remove(pre_trained_model_file)
+        else:
+            pre_trained_model_file = os.path.join(pre_trained_model_dir, prm.model['file_name'])
+            print(f'Downloading the pre-trained model {str(Path(pre_trained_model_file).name)}...')
+            request.urlretrieve(prm.model['download_path'], pre_trained_model_file) # TODO: show progress
+            print('Done.')
     print(f'Pre-trained model is located at {str(Path(pre_trained_model_dir).resolve())}')
 
 if __name__ == '__main__':
